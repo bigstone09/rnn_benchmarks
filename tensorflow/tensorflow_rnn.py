50fa994b2caa3751e476c1b5754ba3ff959b7ef7
= <luke.metz@students.olin.edu>
2016-02-27 23:03:21 -0500

    theano and tensorflow initial

---
+++ tensorflow/tensorflow_rnn.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.models.rnn import rnn
+from tensorflow.models.rnn.rnn_cell import BasicLSTMCell, LSTMCell
+import numpy as np
+import time
+
+if __name__ == '__main__':
+    np.random.seed(1)
+    size = 100
+    batch_size= 128
+    n_steps = 64
+    seq_width = 128
+    n_vocab = 10000
+    n_class = 5
+
+    initializer = tf.random_uniform_initializer(-1,1)
+
+    seq_input = tf.placeholder(tf.int32, [n_steps, batch_size])
+    y = tf.placeholder(tf.int64, [batch_size, ])
+    #sequence we will provide at runtime
+    early_stop = tf.placeholder(tf.int32)
+    #what timestep we want to stop at
+    embedding_matrix = tf.Variable(tf.zeros([n_vocab, seq_width]))
+    W_class = tf.Variable(tf.zeros([seq_width, n_class]))
+    B_class = tf.Variable(tf.zeros([n_class, ]))
+
+    embed_input = tf.nn.embedding_lookup(embedding_matrix, seq_input)
+
+    inputs = [tf.reshape(i, (batch_size, seq_width)) for i in tf.split(0, n_steps, embed_input)]
+    #inputs for rnn needs to be a list, each item being a timestep.
+    #we need to split our input into each timestep, and reshape it because split keeps dims by default  
+
+    #cell = LSTMCell(seq_width, seq_width, initializer=initializer)
+    cell = LSTMCell(seq_width, seq_width, initializer=initializer)
+    initial_state = cell.zero_state(batch_size, tf.float32)
+    outputs, states = rnn.rnn(cell, inputs, initial_state=initial_state, sequence_length=early_stop)
+
+    last = outputs[-1]
+    logit = tf.matmul(last, W_class) + B_class
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logit, y))
+
+    iop = tf.initialize_all_variables()
+    #create initialize op, this needs to be run by the session!
+    session = tf.Session()
+    session.run(iop)
+      #actually initialize, if you don't do this you get errors about uninitialized stuff
+
+    feed = {early_stop:n_steps,
+            seq_input:np.random.randint(0, n_vocab, (n_steps, batch_size)).astype('int32'),
+            y:np.random.randint(0, n_class, (batch_size)).astype("int64")}
+      #define our feeds.
+      #early_stop can be varied, but seq_input needs to match the shape that was defined earlier
+
+    opt = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+
+    #warmup
+    for x in range(2):
+        outs = session.run(opt, feed_dict=feed)
+
+    t_start = time.time()
+    for i in range(30):
+      outs = session.run(opt, feed_dict=feed)
+    print "10 steps took:", time.time() - t_start
---
+++ theano/basic_theano.py
@@ -0,0 +1,131 @@
+import six.moves.cPickle as pickle
+
+from collections import OrderedDict
+import sys
+import time
+
+import numpy as np
+import theano
+from theano import config
+import theano.tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+
+#theano.config.compute_test_value = 'warn'
+
+# Set the random number generators' seeds for consistency
+SEED = 123
+np.random.seed(SEED)
+
+def floatX(data):
+    return np.asarray(data, dtype=config.floatX)
+
+def param(shape, name):
+    return theano.shared(floatX(np.random.randn(*shape)*0.2), name=name)
+
+proj_dim = 128
+embed_dim = 128
+n_class = 5
+n_vocab = 10000
+
+W_class = param((proj_dim, n_class), "W_class")
+B_class = param((n_class, ), "B_class")
+
+W_embed = param((n_vocab, embed_dim), "W_embed")
+W_hh = param((proj_dim, proj_dim*4), "W_hh")
+
+# for 4 lstm gates
+W_toinp = param((proj_dim, proj_dim*4), "W_toinp")
+B_toinp = param((proj_dim*4, ), "B_toinp")
+
+def lstm_layer(inp, mask):
+
+    def _slice(_x, n, dim):
+        if _x.ndim == 3:
+            return _x[:, :, n * dim:(n + 1) * dim]
+        return _x[:, n * dim:(n + 1) * dim]
+
+    def _step(m_, x_, h_, c_):
+        preact = T.dot(h_, W_hh) + x_
+
+        i = T.nnet.sigmoid(_slice(preact, 0, proj_dim))
+        f = T.nnet.sigmoid(_slice(preact, 1, proj_dim))
+        o = T.nnet.sigmoid(_slice(preact, 2, proj_dim))
+        c = T.tanh(_slice(preact, 3, proj_dim))
+
+        c = f * c_ + i * c
+
+        h = o * T.tanh(c)
+
+        # masking
+        c = m_[:, None] * c + (1. - m_)[:, None] * c_
+        h = m_[:, None] * h + (1. - m_)[:, None] * h_
+
+        return h, c
+
+    rval, updates = theano.scan(_step,
+                                sequences=[mask, inp],
+                                outputs_info=[T.alloc(floatX(0.),
+                                                           inp.shape[1],
+                                                           proj_dim),
+                                              T.alloc(floatX(0.),
+                                                           inp.shape[1],
+                                                           proj_dim)],
+                                name="lstm_scan")
+    return rval[0]
+
+def build_model():
+    trng = RandomStreams(SEED)
+
+    x = T.matrix('x', dtype='int64')
+    x.tag.test_value = np.random.randint(0, n_vocab, size=(64, 128))
+
+    mask = T.matrix('mask', dtype=config.floatX)
+    mask.tag.test_value = np.ones((64, 128), dtype="float32")
+    y = T.vector('y', dtype='int64')
+    y.tag.test_value = np.random.randint(0, n_class, size=(128,))
+
+    n_timesteps = x.shape[0]
+    n_samples = x.shape[1]
+
+    embed = W_embed[x.flatten()].reshape([n_timesteps,
+                                        n_samples,
+                                        embed_dim])
+    toinp = (T.dot(embed, W_toinp) + B_toinp)
+
+    output = lstm_layer(toinp, mask)
+
+    last = output[0]
+
+    pred = T.nnet.softmax(T.dot(last, W_class) + B_class)
+
+    cost = T.nnet.categorical_crossentropy(pred, y).mean()
+    params = [W_class, B_class, W_embed, W_hh]
+
+    updates = []
+    for p, g in zip(params, theano.gradient.grad(cost, params)):
+        updates.append((p, p-g*0.001))
+
+    return x, y, mask, cost, pred, updates
+
+
+def benchmark_lstm():
+    print "starting to build model"
+    t_start = time.time()
+    x, y, mask, cost, pred, updates = build_model()
+    f_update_shared = theano.function([x, mask, y], cost, updates=updates,
+                                    name='full')
+    print "done, took:", time.time() - t_start
+    t_start = time.time()
+
+    x_val = np.random.randint(0, n_vocab, size=(64, 128))
+    y_val = np.random.randint(0, n_class, size=(128,))
+    mask_val = np.ones((64, 128), dtype="float32")
+    for i in range(30):
+        print i
+        cost_val = f_update_shared(x_val, mask_val, y_val)
+    print "done with 10 loop, took:", time.time() - t_start
+
+if __name__ == '__main__':
+    benchmark_lstm()
+    import ipdb; ipdb.set_trace()
+
